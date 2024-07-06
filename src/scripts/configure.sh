83ed7165971f38e96becdc3e39490995700ef991
Hima-Pro <pnpop01027066161@gmail.com>
2024-07-06 03:38:26 +0300

    update v2.0.0 fix entrypoint.rc1

--- src/scripts/configure.sh
+++ src/scripts/configure.sh
@@ -11,24 +11,24 @@ case "$1" in
           echo $MYSQL_ROOT_PASSWORD > /app/mysql-root-pw.txt
           tfile=`mktemp`
           
-          printf "
-            USE mysql;
-            FLUSH PRIVILEGES ;
-            GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-            GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-            SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
-            DROP DATABASE IF EXISTS test ;
-            FLUSH PRIVILEGES ;
-            " > $tfile
+          cat << EOF > $tfile
+USE mysql ;
+FLUSH PRIVILEGES ;
+GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+DROP DATABASE IF EXISTS test ;
+FLUSH PRIVILEGES ;
+EOF
           /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tfile
           rm -f $tfile
         ;;
     web)
-        printf "
-          max_file_uploads = 1024
-          upload_max_filesize = 1024M
-          post_max_size = 1024M
-          " > /etc/php83/conf.d/custom.ini
+        cat << EOF > /etc/php83/conf.d/custom.ini
+max_file_uploads = 1024
+upload_max_filesize = 1024M
+post_max_size = 1024M
+EOF
         sed -i "/LoadModule rewrite_module/s/^#//g" /etc/apache2/httpd.conf
         sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/httpd.conf
         ;;
--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -8,7 +8,6 @@ fi
 configure() {
     if [ -f /app/scripts/configure.sh ]; then
         sh /app/scripts/configure.sh $@
-        rm /app/scripts/configure.sh
     fi
 }
 
@@ -16,14 +15,17 @@ case "$1" in
     all)
         configure web
         configure mariadb
+        rm -f /app/scripts/configure.sh
         /usr/bin/supervisord -c /app/configs/supervisord.conf
         ;;
     mariadb)
         configure mariadb
+        rm -f /app/scripts/configure.sh
         /usr/bin/supervisord -c /app/configs/supervisord-mariadb.conf
         ;;
     web)
         configure web
+        rm -f /app/scripts/configure.sh
         /usr/bin/supervisord -c /app/configs/supervisord-web.conf
         ;;
     *)
